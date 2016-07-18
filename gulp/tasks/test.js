8a7535dc6b8e7eb2c2edbf8ac7082aed998cbab5
jfusco <jfusco@wreckingballmedia.com>
2016-07-18 12:10:17 -0400

    Fixing Travis CI integration

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -3,7 +3,7 @@
 module.exports = (gulp, $) => {
 	gulp.task('test', () => {
 		return gulp.src('./src/js/Tags.js', {read: false})
-			.pipe($.shell('jest --coverage'));
+			.pipe($.shell('npm test'));
 	});
 
 	gulp.task('test-dev', () => {
