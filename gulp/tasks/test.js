80acefbe7787f662073d1f17c3d9af77e13a0d1a
jfusco <jfusco@wreckingballmedia.com>
2016-07-18 11:36:06 -0400

    Further cleanup of travis integration

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -3,7 +3,7 @@
 module.exports = (gulp, $) => {
 	gulp.task('test', () => {
 		return gulp.src('./src/js/Tags.js', {read: false})
-			.pipe($.shell('npm test'));
+			.pipe($.shell('jest --coverage'));
 	});
 
 	gulp.task('test-dev', () => {
